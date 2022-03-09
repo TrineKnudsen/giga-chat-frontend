74ee50483a0ca578f953f3954fdcbe55cb0e58d7
TrineKnudsen <trinesaknudsen@hotmail.com>
2022-03-09 08:49:53 +0100

    Initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h1>Hello You are home</h1>
 </template>
